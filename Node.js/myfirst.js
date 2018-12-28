674d72d2cba888d0605d9bc8230ecee724ec09b1
Zander Nickle <znickle24@icloud.com>
2018-12-27 17:32:19 -0700

    Working with npm modules and parsing uri's.

--- Node.js/myfirst.js
+++ Node.js/myfirst.js
@@ -1,6 +1,11 @@
+//similar to importing or including a library
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var uc = require('upper-case');
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write(uc("Hello World!"));
+  return res.end();
 }).listen(8080);
